3ec2c50bbff6b9012d45e51a79bc3f8e6f493c00
Andres <andres@localhost.localdomain>
2018-02-24 16:06:15 -0300

    Adding POST

--- src/main/java/transfmodelo/endpoint.java
+++ src/main/java/transfmodelo/endpoint.java
@@ -1,11 +1,15 @@
 package transfmodelo;
 
 import javax.ws.rs.Path;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import org.json.XML;
+import org.json.JSONObject;
 
-@Path("endpoint")
+@Path("xml2json")
 public class endpoint {
 
 	@GET
@@ -13,4 +17,14 @@ public class endpoint {
 	public String getDataJSON() {
 		return "It works the transf!";
 	}
+	
+	@POST
+	@Produces(MediaType.APPLICATION_JSON)
+	@Consumes(MediaType.APPLICATION_XML)
+	public JSONObject xmlToJSON(String data) {
+		JSONObject xmlJSONObj = XML.toJSONObject(data);
+		System.out.println(xmlJSONObj.toString());
+		return xmlJSONObj;
+	}
+	
 }
