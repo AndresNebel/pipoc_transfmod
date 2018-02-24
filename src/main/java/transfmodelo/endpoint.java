7d3b6f2c7dfaa7d7745918f94ae3320caee2122c
Andres <andres@localhost.localdomain>
2018-02-24 20:02:10 -0300

    Intento de agregar RabbitMQ

--- src/main/java/transfmodelo/endpoint.java
+++ src/main/java/transfmodelo/endpoint.java
@@ -9,9 +9,19 @@ import javax.ws.rs.core.MediaType;
 import org.json.XML;
 import org.json.JSONObject;
 
+import java.io.IOException;
+import java.util.concurrent.*;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.Consumer;
+import com.rabbitmq.client.DefaultConsumer;
+import com.rabbitmq.client.Envelope;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.AMQP;
+import com.rabbitmq.client.Channel;
+
 @Path("xml2json")
 public class endpoint {
-
+	public static String nextStepName = ""; 
 	@GET
 	@Produces(MediaType.TEXT_PLAIN)
 	public String getDataJSON() {
@@ -27,4 +37,38 @@ public class endpoint {
 		return xmlJSONObj.toString();
 	}
 	
+	@Path("initMQ")
+	@POST
+	public void initializeMessageQueue() {
+		System.out.println("transfmodelo: Initializing MQ");
+		ExecutorService executor = Executors.newFixedThreadPool(1);
+		executor.submit(new Runnable() {
+			public void run() {
+				System.out.println("transfmodelo: RabbitMQ Receiver Thread initializing..");
+				ConnectionFactory factory = new ConnectionFactory();
+				factory.setHost("localhost");
+				Connection connection;
+				try {
+					connection = factory.newConnection();
+					Channel channel = connection.createChannel();
+					channel.queueDeclare("transfmodelo", false, false, false, null);
+					System.out.println("transfmodelo: Queue declared, adding consumer...");
+					Consumer consumer = new DefaultConsumer(channel) {
+					  @Override
+					  public void handleDelivery(String consumerTag, Envelope envelope,
+					                             AMQP.BasicProperties properties, byte[] body)
+					      throws IOException {
+					    String message = new String(body, "UTF-8");
+					    System.out.println(" [x] Received '" + message + "'");
+					  }
+					};
+					channel.basicConsume("transfmodelo", true, consumer);
+					System.out.println("transfmodelo: All set, waiting for messages now.");
+				} catch (IOException | TimeoutException e) {					
+					e.printStackTrace();
+				}
+				
+			}
+		});
+	}
 }
